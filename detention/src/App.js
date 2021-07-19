366bac80fb08bd1f6f5a8497c8b8af0af3a3da98
jwshuff <16825267+JWShuff@users.noreply.github.com>
2021-07-19 14:56:02 -0400

    used the app-header class to get to a black background. Looking good for rel 2.

--- detention/src/App.js
+++ detention/src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
 
   render() {
     return (
-      <div className='App'>
+      <div className='App-header'>
         { this.createMessages() }
       </div>
     )
